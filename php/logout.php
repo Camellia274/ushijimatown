bc850ca17a0c1fca5740225be52ccb5951e43ef5
admin <admin@ShotaUshijima>
2015-06-30 00:52:09 +0900

    ログイン・ログアウト完成

--- php/logout.php
+++ php/logout.php
@@ -1,4 +1,12 @@
 <?php
+	//セッションを開始する
+	session_start();
+
+	//ユーザ名とユーザIDのセッションを削除する
+	if (isset($_SESSION['username']) && isset($_SESSION['userid'])){
+		unset($_SESSION['username']);
+		unset($_SESSION['userid']);
+	}
 
 	// セッション変数を全て解除する
 	$_SESSION = array();
